d49e30f9e88bb66ab499cf019a8478ab9bc45871
Stanley Lin <slin55055@gmail.com>
2017-10-17 23:42:05 -0400

    im tired

--- include/music_library.h
+++ include/music_library.h
@@ -11,7 +11,7 @@ struct node* find_by_song_name_artist(struct node **lib,
 	char *artist, char *song);
 struct node* find_by_artist(struct node **lib, char *artist);
 
-void print_artist_by_letter(struct node **lib, char letter);
+void print_all_by_letter(struct node **lib, char letter);
 void print_library(struct node **lib);
 void shuffle(struct node **lib);
 
--- lib/music_library.c
+++ lib/music_library.c
@@ -18,30 +18,65 @@ int get_letter_index(char *str) {
 
 void add_song(struct node **lib, char *artist, char *song) {
 	int index = get_letter_index(artist);
-	struct node *ptr;
-	
 	
 	if (!lib[index] || strncasecmp(artist, lib[index]->artist, strlen(artist)+1) < 0) {
 		lib[index] = insert_front(lib[index], artist, song);
+		return;
 	}
 	else if (!strncasecmp(artist, lib[index]->artist, strlen(artist)+1)) {
 		if (strncasecmp(song, lib[index]->song, strlen(song)+1) <= 0) {
 			lib[index] = insert_front(lib[index], artist, song);
 		}
-		else {
+		return;
+	}
+	
+	if(!(head[index]->next)) {
+		if (strncasecmp(artist, head[index]->artist, strlen(artist)+1 > 0 ||
+				strncasecmp(song, head[index]->song, strlen(song)+1 > 0) {
 			lib[index] = insert_at(lib[index], 1, artist, song);
 		}
-	}
-	else {
-		ptr = lib[index];
-		int counter = 0;
-		while(ptr->next) {
-			break;
-			//if (str
+		else {
+			lib[index] = insert_front(lib[index], artist, song);
 		}
+		return;
 	}
+	
+	struct node *hi = lib[index]->next;
+	struct node *lo = lib[index];
+	int cmp_res, counter = 1;
+	while(hi) {
+		cmp_res = strncasecomp(artist, hi->artist, strlen(artist)+1);
+		
+		if(cmp_res < 0) {
+			lo->next = new_node(artist, song, hi);
+			return;
+		}
+		else if (cmp_res == 0) {
+			lo = hi;
+			hi = hi->next;
+			
+			//now check the songs, use the same procedure
+			while(hi) {
+				
+				
+				lo = lo->next;
+				hi = hi->next;
+			}
+		}
+		
+		lo = lo->next;
+		hi = hi->next;
+	}
+	
+	lo->next = new_node(artist, song, hi);
 }
 
+void print_all_by_letter(struct node **lib, char letter) {
+	printf("Artists under %c\n", letter);
+	
+	int index = get_letter_index(&letter);
+	print_list(lib[index]);
+}
 
 
 void print_library(struct node **lib) {
@@ -54,4 +89,10 @@ void print_library(struct node **lib) {
 }
 
 
+void delete_all_songs(struct node **lib) {
+	int x;
+	for (x = 0; x < MUSIC_LIB_LEN; x++) {
+		lib[x] = free_list(lib[x]);
+	}
+}
 
